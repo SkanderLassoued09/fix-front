f60147871f7634c9a179c46c58cd5e5cdff33d52
MrNezihBJ <nezihbenjemia15@gmail.com>
2025-07-16 10:28:19 +1300

    ip added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/', // Replace "/api" with your actual API endpoint
-    host: 'http://localhost:4200', // Front-end host
+    apiUrl: 'http://192.168.1.29:3000/', // Replace "/api" with your actual API endpoint
+    host: 'http://192.168.1.29:4200', // Front-end host
 };
 //! note to skander to access files i've added "/" to apiUrl so I've deleted the "/" in graphql.modules.ts and you can use the comment below to find them
 //?  //changed apiUrl
